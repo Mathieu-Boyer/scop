ed05f0ad2b4f0cddb58585d0901a1c926e7d8bb3
EZ3C <mboyer980@gmail.com>
2025-11-05 13:31:05 +0100

    working triangle

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,14 @@ const char *fragmentShaderSource =
 "#version 330 core\n"
 "out vec4 fragColor;\n"
 "void main(){"
-"fragColor = vec4(.4, .2, .8, 1.0);"
+"fragColor = vec4(.1, .2, .8, 1.0);"
 "}\0";
+
+const GLfloat verticies[] = {
+    -0.5f, -0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    0.0f, 0.5f , 0.0f
+};
 
 int main (){
     glfwInit();
@@ -43,6 +49,31 @@ int main (){
     glAttachShader(shaderProgram, fragmentShader);
     glLinkProgram(shaderProgram);
 
+
+    GLuint VBO, VAO;
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3 ,(void *)0);
+    glEnableVertexAttribArray(0);
+
+
+    while(!glfwWindowShouldClose(window)){
+        glfwPollEvents();
+        glClearColor(.1, .2, .4, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0,3);
+
+        glfwSwapBuffers(window);
+    }
 
     glfwDestroyWindow(window);
     glfwTerminate();
