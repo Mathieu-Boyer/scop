576301730b14a70f7932c17b0568aa0f1802ef1c
EZ3C <mboyer980@gmail.com>
2025-11-05 13:14:54 +0100

    adding basic fragment shader

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,32 @@ const char *vertexShaderSource =
 "gl_Position = vec4(aPos, 1.0);"
 "}\0";
 
+const char *fragmentShaderSource = 
+"#version 330 core\n"
+"out vec4 fragColor;\n"
+"void main(){"
+"fragColor = vec4(.4, .2, .8, 1.0);"
+"}\0";
+
 int main (){
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 
+    GLFWwindow *window = glfwCreateWindow(800, 800, "Scop", NULL, NULL);
+
+    if (!window){
+        std::cerr << "Window couldn't be openned\n";
+        glfwTerminate();
+    }
+
+    glfwMakeContextCurrent(window);
+
+
+
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 }
