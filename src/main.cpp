91ec581ea9de6640904b515bec1c05a1395941df
EZ3C <mboyer980@gmail.com>
2025-11-05 11:15:38 +0100

    adding basic vertex shader

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
 #include "main.hpp"
 
+const char *vertexShaderSource = 
+"#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main(){"
+"gl_Position = vec4(aPos, 1.0);"
+"}\0";
+
 int main (){
+
 
 }
