e1e6509e829406c661fcfa558f297dea4f777193
EZ3C <mboyer980@gmail.com>
2025-11-05 13:15:43 +0100

    Making the basic shader program and compilation

--- src/main.cpp
+++ src/main.cpp
@@ -30,7 +30,18 @@ int main (){
 
     glfwMakeContextCurrent(window);
 
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
 
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
 
 
     glfwDestroyWindow(window);
