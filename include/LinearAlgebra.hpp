7dcbb5674143c98bd7575ee3694211c640f2f9b7
Mathieu-Boyer <mboyer980@gmail.com>
2025-11-13 12:47:21 +0100

    for now these are the only basic function i'm thinking of , i'll add more with implementation.

--- include/LinearAlgebra.hpp
+++ include/LinearAlgebra.hpp
@@ -12,18 +12,19 @@ class LinearAlgebra
         struct vec3{
             float x,y,z;
         };
+        struct vec4{
+            float x,y,z,w;
+        };
+
         struct mat4{
             float data[16];
         };
-        
+
+        static vec3 cross(const vec3 &v1, const vec3 &v2);
+        static float dot(const vec3 &a, const vec3 &b);
+        static vec3 dot(const mat4 &m, const vec4 &v);
+
         LinearAlgebra() = delete;
         ~LinearAlgebra() = delete;
 };
 
-// LinearAlgebra::LinearAlgebra(/* args */)
-// {
-// }
-
-// LinearAlgebra::~LinearAlgebra()
-// {
-// }
