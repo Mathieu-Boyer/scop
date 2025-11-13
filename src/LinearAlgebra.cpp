ed066d6d0820965a38ad837f24b7c8af8b4f9e1f
Mathieu-Boyer <mboyer980@gmail.com>
2025-11-13 15:27:28 +0100

    radian conversion

--- src/LinearAlgebra.cpp
+++ src/LinearAlgebra.cpp
@@ -30,12 +30,10 @@ LinearAlgebra::vec3 LinearAlgebra::normalize(const vec3 &v){
 
 LinearAlgebra::vec4 LinearAlgebra::dot(const LinearAlgebra::mat4 &m, const LinearAlgebra::vec4 &v){
     LinearAlgebra::vec4 out;
-
     out.x = m.data[0] * v.x + m.data[1] * v.y + m.data[2] * v.z + m.data[3] * v.w;
     out.y = m.data[4] * v.x + m.data[5] * v.y + m.data[6] * v.z + m.data[7] * v.w;
     out.z = m.data[8] * v.x + m.data[9] * v.y + m.data[10] * v.z + m.data[11] * v.w;
     out.w = m.data[12] * v.x + m.data[13] * v.y + m.data[14] * v.z + m.data[15] * v.w;
-
     return out;
 }
 
