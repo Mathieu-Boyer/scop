1e7cbdd794d18c5dce2548bdcd67c572994683f9
Mathieu-Boyer <mboyer980@gmail.com>
2025-11-15 17:21:07 +0100

    adding macos/linux flags for compilation

--- include/utils.hpp
+++ include/utils.hpp
@@ -5,6 +5,7 @@ float degToRad(float degrees);
 
 
 #ifdef __linux__
+#include <GL/gl.h>
 typedef void (*PFNGLGENERATEMIPMAPPROC)(GLenum);
 PFNGLGENERATEMIPMAPPROC glGenerateMipmapFunc = (PFNGLGENERATEMIPMAPPROC)glXGetProcAddress((const GLubyte*)"glGenerateMipmap");
 
